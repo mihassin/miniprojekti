cef43c37c706994011d47e19024b8cc5d208ee68
esakorte <esa.kortelainen@helsinki.fi>
2014-04-03 19:12:04 +0300

    inbroxedings testejä

--- pohjalimat/src/test/java/wad/pohjalimat/model/InproceedingsTest.java
+++ pohjalimat/src/test/java/wad/pohjalimat/model/InproceedingsTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package wad.pohjalimat.model;
 
 import org.junit.After;
@@ -18,25 +17,38 @@ import static org.junit.Assert.*;
  * @author Juhani Heliö
  */
 public class InproceedingsTest {
-    
+
     Inproceedings ip;
     Inproceedings mouckaPhii;
-    
+    Inproceedings elGiganto;
+
     public InproceedingsTest() {
     }
 
     @Before
     public void setUp() {
-        ip=new Inproceedings("viite1","Pena", "Muistio", "Penan muistio", 2000);
-        mouckaPhii = Inproceedings.create("viite1","Pena", "Muistio", "Penan muistio", 2000);
+        ip = new Inproceedings("viite1", "Pena", "Muistio", "Penan muistio", 2000);
+        mouckaPhii = Inproceedings.create("viite1", "Pena", "Muistio", "Penan muistio", 2000);
+        elGiganto = Inproceedings.create("giganto1", "Senor Gigante", "El Giganto", "Suuri mies hän on", 2014);
+        elGiganto.setEditor("testeditor");
+        elGiganto.setVolume(9999);
+        elGiganto.setSeries("testseries");
+        elGiganto.setPages("10--20");
+        elGiganto.setAddress("testaddress");
+        elGiganto.setMonth("January");
+        elGiganto.setPublisher("testpublisher");
+        elGiganto.setOrganisation("testorganisation");
+        elGiganto.setNote("testnote");
+        elGiganto.setKey("testkey");
+
     }
-    
+
     /**
      * Test of create method, of class Inproceedings.
      */
     @Test
     public void testCreate() {
-        assertEquals(ip, Inproceedings.create("Viite1","Pena", "Muistio", "Penan muistio", 2000));
+        assertEquals(ip, Inproceedings.create("Viite1", "Pena", "Muistio", "Penan muistio", 2000));
     }
 
     /**
@@ -44,7 +56,7 @@ public class InproceedingsTest {
      */
     @Test
     public void testGetAuthor() {
-        assertEquals( "Pena", ip.getAuthor() ); //slime-marko
+        assertEquals("Pena", ip.getAuthor()); //slime-marko
     }
 
     /**
@@ -52,7 +64,7 @@ public class InproceedingsTest {
      */
     @Test
     public void testGetTitle() {
-        assertEquals( "Muistio", ip.getTitle() ); // slime-make
+        assertEquals("Muistio", ip.getTitle()); // slime-make
     }
 
     /**
@@ -60,8 +72,8 @@ public class InproceedingsTest {
      */
     @Test
     public void testGetBooktitle() {
-        
-        assertEquals( "Penan muistio", ip.getBooktitle() ); //slime-kake
+
+        assertEquals("Penan muistio", ip.getBooktitle()); //slime-kake
     }
 
     /**
@@ -69,102 +81,162 @@ public class InproceedingsTest {
      */
     @Test
     public void testGetYear() {
-        
-        assertEquals( 2000, ip.getYear() ); //slimeM
+
+        assertEquals(2000, ip.getYear()); //slimeM
     }
-    
+
+    @Test
+    public void testGetEditor() {
+        
+        assertEquals("testeditor", elGiganto.getEditor());
+    }
+
+    @Test
+    public void testGetVolume() {
+        
+        assertEquals(9999, elGiganto.getVolume());
+    }
+
+    @Test
+    public void testGetSeries() {
+        
+        assertEquals("testseries", elGiganto.getSeries());
+    }
+
+    @Test
+    public void testGetPages() {
+        
+        assertEquals("10--20", elGiganto.getPages());
+    }
+
+    @Test
+    public void testGetAddress() {
+        
+        assertEquals("testaddress", elGiganto.getAddress());
+    }
+
+    @Test
+    public void testGetMonth() {
+        
+        assertEquals("January", elGiganto.getMonth());
+    }
+
+    @Test
+    public void testGetPublisher() {
+        
+        assertEquals("testpublisher", elGiganto.getPublisher());
+    }
+
+    @Test
+    public void testGetOrganisation() {
+        
+        assertEquals("testorganisation", elGiganto.getOrganisation());
+    }
+
+    @Test
+    public void testGetNote() {
+        
+        assertEquals("testnote", elGiganto.getNote());
+    }
+
+    @Test
+    public void testGetKey() {
+        
+        assertEquals("testkey", elGiganto.getKey());
+    }
+
     /* slimy-marko */
     @Test
     public void equalsMethodObjNull() {
         assertFalse(ip.equals(null));
     }
-    
+
     @Test
     public void selfEqTrueOFC() {
         assertTrue(ip.equals(ip));
     }
-   
+
     @Test
     public void wiffMockClassTru() {
         assertTrue(ip.equals(mouckaPhii));
     }
-    
+
     @Test
     public void wiffMockClassSetNewAuth() {
         mouckaPhii.setAuthor("slime");
         assertFalse(ip.equals(mouckaPhii));
     }
-    
+
     @Test
     public void wiffMockClassSetNewTitle() {
         mouckaPhii.setTitle("slime");
         assertFalse(ip.equals(mouckaPhii));
     }
-    
+
     @Test
     public void wiffMockClassSetNewBookTitle() {
         mouckaPhii.setBooktitle("slime");
         assertFalse(ip.equals(mouckaPhii));
     }
-    
+
     @Test
     public void wiffMockClassSetNewYarr() {
         mouckaPhii.setYear(1234);
         assertFalse(ip.equals(mouckaPhii));
     }
-    
+
     @Test
     public void stringgingInprocedding() {
         assertFalse(ip.equals(123));
     }
-    
+
     @Test
     public void gettinNSettinAuth() {
         ip.setAuthor("slime");
-        
+
         assertEquals("slime", ip.getAuthor());
     }
-    
+
     @Test
     public void gettinNSettinTitle() {
         ip.setTitle("slime");
-        
+
         assertEquals("slime", ip.getTitle());
     }
-    
+
     @Test
     public void gettinNSettinBookTitle() {
         ip.setBooktitle("slime");
-        
+
         assertEquals("slime", ip.getBooktitle());
     }
-    
+
     @Test
     public void gettinNSettinYARR() {
         ip.setYear(1234);
-        
+
         assertEquals(1234, ip.getYear());
     }
-    
+
     @Test
     public void freshHash() {
         int hashh = ip.hashCode();
-        
+
         assertEquals(hashh, ip.hashCode());
     }
-    
+
     @Test
     public void unFreshHash() {
         assertNotSame(ip.hashCode(), mouckaPhii.hashCode());
     }
-    
+
     @Test
     public void testToString() {
         String testString = "*** Muistio ***\nAuthor: Pena\n"
-                            + "Book title: Penan muistio\n"
-                            + "Year published: 2000\n";
-        
+                + "Book title: Penan muistio\n"
+                + "Year published: 2000\n";
+
         assertEquals(testString, ip.toString());
     }
-    
+
 }
