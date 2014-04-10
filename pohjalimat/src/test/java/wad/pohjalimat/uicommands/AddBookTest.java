7dd5cb0670236f7a090163b178df7e5bc701928c
esakorte <esa.kortelainen@helsinki.fi>
2014-04-10 22:05:03 +0300

    saven junit pois kun easyb hoitaa, source format addbooktestille

--- pohjalimat/src/test/java/wad/pohjalimat/uicommands/AddBookTest.java
+++ pohjalimat/src/test/java/wad/pohjalimat/uicommands/AddBookTest.java
@@ -43,8 +43,8 @@ public class AddBookTest {
 
         assertTrue(testIO.getOutput().contains("Invalid entry"));
     }
-    
-        @Test
+
+    @Test
     public void addingANewEntryWithInvalidInput2() {
         testIO = new StubIO("aaafs", "", "work", "publisher", "2014", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
@@ -52,8 +52,8 @@ public class AddBookTest {
 
         assertTrue(testIO.getOutput().contains("Invalid entry"));
     }
-        
-                @Test
+
+    @Test
     public void addingANewEntryWithInvalidInput3() {
         testIO = new StubIO("aaafs", "", "work", "publisher", "-1", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
@@ -61,8 +61,8 @@ public class AddBookTest {
 
         assertTrue(testIO.getOutput().contains("Invalid entry"));
     }
-                
-                                @Test
+
+    @Test
     public void addingANewEntryWithInvalidInput4() {
         testIO = new StubIO("aaafs", "", "work", "publisher", "hess", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
@@ -70,25 +70,25 @@ public class AddBookTest {
 
         assertTrue(testIO.getOutput().contains("Invalid entry"));
     }
-        
-            @Test
-    public void addingANewEntryWithDuplicateMasterKeyIsHandled () {
+
+    @Test
+    public void addingANewEntryWithDuplicateMasterKeyIsHandled() {
         testIO = new StubIO("masterkey", "writerguy", "work", "work list", "2014", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
         testAddBook.run();
-        
+
         testIO = new StubIO("just testing masterkey", "writerguy", "work", "work list", "2014", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
         testAddBook.run();
-        
+
         testIO = new StubIO("masterkey", "writerguy", "work", "work list", "2014", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
         testAddBook.run();
-        
+
         testIO = new StubIO("masterkey", "writerguy", "work", "work list", "2014", "2", "b", "a", "b", "a", "b", "a");
         testAddBook = new AddBook(testIO, testRefList);
         testAddBook.run();
-        
+
         assertTrue(testRefList.get(0).getMasterKey().equals("masterkey"));
         assertTrue(testRefList.get(2).getMasterKey().equals("masterkey__1"));
         assertTrue(testRefList.get(3).getMasterKey().equals("masterkey__2"));
