317cda4d4e0fa3293580b163a4ace2cb98279090
mihassin <marko.hassinen@helsinki.fi>
2014-04-24 18:45:54 +0300

    clean up

--- pohjalimat/src/main/java/wad/pohjalimat/controller/DefaultControllerInterface.java
+++
@@ -1,8 +0,0 @@
-package wad.pohjalimat.controller;
-
-import org.springframework.ui.Model;
-
-public interface DefaultControllerInterface {
-    String read(String message);
-    String write(Model model);
-}
